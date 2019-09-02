e18d8890d86f4db64b3886d33cf72b86e1844cf0
Matheus Mortatti <matheusmortatti@gmail.com>
2019-09-02 18:42:08 -0300

    added template, missing audio

---
+++ Assets/pico8Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pico8_Emulator;
+using UnityEngine.UI;
+
+public class pico8Controller : MonoBehaviour
+{
+    public RawImage rawImage;
+
+    private Color[] screenColorData;
+    Pico8<Color> pico8;
+    Texture2D screenTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 30;
+
+        pico8 = new Pico8<Color>();
+        screenColorData = new Color[128 * 128];
+        screenTexture = new Texture2D(128, 128, TextureFormat.RGBA32, false, true);
+        screenTexture.filterMode = FilterMode.Point;
+
+        pico8.screenColorData = screenColorData;
+        pico8.rgbToColor = (r, g, b) => new Color(r / 255.0f, g / 255.0f, b / 255.0f);
+        pico8.AddLeftButtonDownFunction(() => { return Input.GetKey(KeyCode.LeftArrow); }, 0);
+        pico8.AddRightButtonDownFunction(() => { return Input.GetKey(KeyCode.RightArrow); }, 0);
+        pico8.AddUpButtonDownFunction(() => { return Input.GetKey(KeyCode.UpArrow); }, 0);
+        pico8.AddDownButtonDownFunction(() => { return Input.GetKey(KeyCode.DownArrow); }, 0);
+        pico8.AddOButtonDownFunction(() => { return Input.GetKey(KeyCode.Z); }, 0);
+        pico8.AddXButtonDownFunction(() => { return Input.GetKey(KeyCode.X); }, 0);
+
+        pico8.LoadGame("Assets/jelpi.p8", new NLuaInterpreter());
+
+        rawImage.texture = screenTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pico8.Draw();
+
+        screenTexture.SetPixels(screenColorData);
+        screenTexture.Apply();
+    }
+
+    void FixedUpdate()
+    {
+        pico8.Update();
+    }
+}
